6335d4e58c939455533b7434e86359de5fe426b5
cassio.korogui <external.cassio.korogui@kuehne-nagel.com>
2023-01-06 10:57:26 -0300

    Fix persons migration script

--- spring-app/src/main/resources/db/migration/V0.0.1_2__migrate_persons.sql
+++ spring-app/src/main/resources/db/migration/V0.0.1_2__migrate_persons.sql
@@ -2,4 +2,4 @@ INSERT INTO persons (id, name) VALUES (1, 'John Rain');
 INSERT INTO persons (id, name) VALUES (2, 'Louis Kent');
 INSERT INTO persons (id, name) VALUES (3, 'Julia Snow');
 INSERT INTO persons (id, name) VALUES (4, 'William Roberts');
-INSERT INTO persons (id, name) VALUES (5, 'Madonna Eintein');
+INSERT INTO persons (id, name) VALUES (5, 'Madonna Einstein');
