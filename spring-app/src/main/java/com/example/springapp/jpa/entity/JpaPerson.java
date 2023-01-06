974d0be4185ce81f97425a719d768e3358481d96
cassio.korogui <external.cassio.korogui@kuehne-nagel.com>
2023-01-06 13:43:28 -0300

    downgrade java 17 to 11

--- spring-app/src/main/java/com/example/springapp/jpa/entity/JpaPerson.java
+++ spring-app/src/main/java/com/example/springapp/jpa/entity/JpaPerson.java
@@ -1,8 +1,8 @@
 package com.example.springapp.jpa.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity(name = "PERSONS")
 public class JpaPerson {
